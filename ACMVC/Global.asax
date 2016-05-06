d44c6b11370d25efa83a551f8da14b83765fe8bc
Zarif <Zarif@localhost>
2016-05-07 03:17:18 +0600

    First Commit

---
+++ ACMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ACMVC.MvcApplication" Language="C#" %>
---
+++ ACMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ACMVC.Startup))]
+namespace ACMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
