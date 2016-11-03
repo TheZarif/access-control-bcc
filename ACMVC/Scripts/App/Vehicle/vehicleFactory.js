53701cb68b527e553ff1155b6c6592f889e15ab1
atiburrahman <atiburrahman@localhost>
2016-11-03 17:25:19 +0600

    Vehicle search done

--- ACMVC/Scripts/App/Vehicle/vehicleFactory.js
+++ ACMVC/Scripts/App/Vehicle/vehicleFactory.js
@@ -10,8 +10,8 @@
             getVehicle: function (page, search) {
                 if (!page) page = 1;
                 if (!search) search = "";
-                return $http.get(baseUrl + "vehicle/getall?page=" + page +"&search="+search);
-            },
+                return $http.get(baseUrl + "vehicle/getall?page=" + page + "&search=" + search);
+               },
             addVehicle: function (vehicle) {
                 return $http.post(baseUrl + "vehicle/create", vehicle);
             },
