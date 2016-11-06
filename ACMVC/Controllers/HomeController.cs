5814a92071126e7cf941047af1a34e4c25a59ca8
atiburrahman <atiburrahman@localhost>
2016-11-06 17:56:43 +0600

    code clean up

--- ACMVC/Controllers/HomeController.cs
+++ ACMVC/Controllers/HomeController.cs
@@ -37,12 +37,7 @@ namespace ACMVC.Controllers
 
             return View();
         }
-        //public ActionResult OfficialsList()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
+      
         public ActionResult OfficialsList()
         {
             OfficialListViewModel officialListViewModel = new OfficialListViewModel();
@@ -64,14 +59,7 @@ namespace ACMVC.Controllers
                 }).ToList()
             }).ToList();
             
-            //List<AspNetUser> users = testEntities.AspNetUsers.ToList();
             List<Official> officials = new List<Official>();
-            //var filter = false;
-            //var isEmployee = false;
-            //if (filter)
-            //{
-            //    users = users.Where(p => p.IsEmployee == isEmployee).ToList();
-            //}
             List<OfficialViewModel> officialViewModels = new List<OfficialViewModel>();
             foreach (var item in designations)
             {
@@ -85,65 +73,11 @@ namespace ACMVC.Controllers
                     officialViewModel.Email = userinfo.Email;
                     officialViewModel.Phone = userinfo.PhoneNumber;
                 }
-                //officialViewModel.OfficialName = "Atib";//item.FullName;
-                //officialViewModel.Designation = "Jr";//item.Designation.ToString();
-                //officialViewModel.Email = "at@test.com";//item.Email;
-                //officialViewModel.Phone = "01671172387";//item.PhoneNumber;
+               
                 officialViewModels.Add(officialViewModel);
             }
-
             officialListViewModel.Officials = officialViewModels;
-
             return View("OfficialsList", officialListViewModel);
-
-            //var pager = new Pager(users.Count(), 1);
-
-            //var viewModel = new UserViewModel()
-            //{
-            //    Users = users.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize).Select(x => new AspNetUser()
-            //    {
-            //        Id = x.Id,
-            //        Email = x.Email,
-            //        PhoneNumber = x.PhoneNumber,
-            //        FullName = x.FullName,
-            //        DeskFloor = x.DeskFloor,
-            //        IsEmployee = x.IsEmployee,
-            //        IsVerified = x.IsVerified,
-            //        EmployeeAccessZoneMaps = x.EmployeeAccessZoneMaps.Select(m => new EmployeeAccessZoneMap
-            //        {
-            //            Id = m.Id,
-            //            UserId = m.UserId,
-            //            AccessZone = new AccessZone
-            //            {
-            //                Id = m.AccessZoneId,
-            //                Name = m.AccessZone.Name
-            //            }
-            //        }).ToList(),
-            //        UserName = x.UserName,
-            //        AspNetRoles = x.AspNetRoles.Select(p => new AspNetRole() { Id = p.Id, Name = p.Name }).ToList()
-            //    }).ToList(),
-            //    Pager = pager
-            //};
-
-            //List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
-
-            //foreach (Employee emp in employees)
-            //{
-            //    EmployeeViewModel empViewModel = new EmployeeViewModel();
-            //    empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
-            //    empViewModel.Salary = emp.Salary.ToString("C");
-            //    if (emp.Salary > 15000)
-            //    {
-            //        empViewModel.SalaryColor = "yellow";
-            //    }
-            //    else
-            //    {
-            //        empViewModel.SalaryColor = "green";
-            //    }
-            //    empViewModels.Add(empViewModel);
-            //}
-            //employeeListViewModel.Employees = empViewModels;
-            //return View("EmployeeList", employeeListViewModel);
         }
 
 
